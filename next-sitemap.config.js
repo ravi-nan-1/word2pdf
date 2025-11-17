ccf411433fb74dd97a680f17fd93ebe5d9f180d8
rajeev singh <singhrajeev1470@gmail.com>
2025-11-17 21:23:37 +0000

    next-sitemap.config.js  add this also module.exports = {   siteUrl: "ht

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://pdf2word.all2ools.com",
+  generateRobotsTxt: true,
+  changefreq: "weekly",
+  priority: 1.0,
+  sitemapSize: 5000,
+};
